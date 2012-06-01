761dcabff04493f19e077ed135c8b9a552684e6c
Shoichi Otomo <p.chang.0129@gmail.com>
2012-06-01 20:38:29 +0900

    LayerSwitcherにマーカーレイヤー追加するのはこれでOK
    ただし、実際のマーカー描画エリアはココに書くとダメ。エスパーデバッグ中。

--- js/all-overlays-google.js
+++ js/all-overlays-google.js
@@ -1,4 +1,5 @@
 var map;
+var marker = new Array();
 
 function init() {
 
@@ -10,11 +11,28 @@ function init() {
     var osm = new OpenLayers.Layer.OSM();
     //使いたいレイヤーはここに追加。スイッチャーで切り替える
     var gmap = new OpenLayers.Layer.Google("Google Streets", {visibility: false});
+    
+    
+    //add marker test
+    var markers = new OpenLayers.Layer.Markers("Markers");
+    /*　ここでマーカーレイヤーにaddMarker出来ない　理由は不明 */
+    marker.addLayer(markers);
+    var marker = new OpenLayers.Marker(
+    new OpenLayers.LonLat(139.76, 35.68)
+        .transform(
+            new OpenLayers.Projection("EPSG:4326"), 
+            new OpenLayers.Projection("EPSG:900913")
+        )
+    );
+    markers.addMarker(marker);
+    */
+//-- ここまで --
 
+    
     //初期状態でosmを選択する。
-    map.addLayers([osm, gmap]);
+    map.addLayers([osm, gmap, markers]);
 
     map.addControl(new OpenLayers.Control.LayerSwitcher());
     map.zoomToMaxExtent();
-
+    
 }
