9de6678656a56880de9b74c518fb9946636bbd53
Shoichi Otomo <p.chang.0129@gmail.com>
2012-06-04 20:33:30 +0900

    setcCenterが無効化されてる謎

--- js/markerLayer.js
+++ js/markerLayer.js
@@ -1,21 +1,22 @@
-        var map, layer;
+var map, layer;
+var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
+var toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
 
         function init(){
             OpenLayers.ProxyHost="/proxy/?url=";
             map = new OpenLayers.Map('map');
             layer = new OpenLayers.Layer.WMS( "OpenLayers WMS", 
                 "http://vmap0.tiles.osgeo.org/wms/vmap0", {layers: 'basic'} );
-//このへんまでできると、表示サイズが変わる。理由は現在究明中
-//イマイチどれがどのマーカーか分からない。                
+                
             map.addLayer(layer);
-            map.setCenter(new OpenLayers.LonLat(0, 0), 0);
-
+            map.setCenter(new OpenLayers.LonLat(139.677493, 35.661127).transform( fromProjection, toProjection);
+/*
             var newl = new OpenLayers.Layer.Text( "text", { location:"./textfile.txt"} );
             map.addLayer(newl);
 
             var markers = new OpenLayers.Layer.Markers( "Markers" );
             map.addLayer(markers);
-//ここの独自定義マーカーいらない
+
             var size = new OpenLayers.Size(21,25);
             var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
             var icon = new OpenLayers.Icon('http://www.openlayers.org/dev/img/marker.png',size,offset);
@@ -28,9 +29,11 @@
             marker.setOpacity(0.2);
             marker.events.register('mousedown', marker, function(evt) { alert(this.icon.url); OpenLayers.Event.stop(evt); });
             markers.addMarker(marker); 
+*/            
+            
             map.addControl(new OpenLayers.Control.LayerSwitcher());
             map.zoomToMaxExtent();
 
-            halfIcon.setOpacity(0.5);
+//            halfIcon.setOpacity(0.5);
             
         }
